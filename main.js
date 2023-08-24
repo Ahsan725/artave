80d41e39518c1e627edb4890e92cfa092c87786e
Ahsan <sbaseer25@gmail.com>
2023-08-24 01:30:18 -0400

    more changes

--- main.js
+++ main.js
@@ -22,3 +22,11 @@ window.onscroll = () => {
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container2");
+
+sign_up_btn.addEventListener("click", () => {
+    container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => { 
+    container.classList.remove("sign-up-mode");
+});
