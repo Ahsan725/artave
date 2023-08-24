baf6128eec2ef95cfd1c464574be8ed53871242a
Ahsan <sbaseer25@gmail.com>
2023-08-24 01:56:19 -0400

    more changes

--- main.js
+++ main.js
@@ -16,17 +16,3 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navbar.classList.remove('open');
 };
-
-// script for the account Sign/Log In page
-
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container2");
-
-sign_up_btn.addEventListener("click", () => {
-    container.classList.add("sign-up-mode");
-});
-
-sign_in_btn.addEventListener("click", () => { 
-    container.classList.remove("sign-up-mode");
-});
